6522a9eede92220acffecd6375c0983a01649806
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-08-03 21:09:29 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-content">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
